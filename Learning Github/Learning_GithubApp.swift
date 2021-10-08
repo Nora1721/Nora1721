2f6e45bc17b55a55ee4544e58a7cb3b9e0a67d99
amelia <arivers2021@icloud.com>
2021-10-07 17:36:45 -0700

    Initial Commit

---
+++ Learning Github/Learning_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GithubApp.swift
+//  Learning Github
+//
+//  Created by Nora Rivers on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
